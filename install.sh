c5d4db9566b1e71acfa2b70147e09292d6a481a8
Rixon cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-04-12 16:55:08 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear 
+Pkg install git
+Pkg install python
+termux-setup-storage
+pkg install curl
+pkg install openssh
+pkg install php
+figlet -f big FINISHED !!! | lolcat
+echo
+echo -e "\e[1m Now install termux basic packages!!!"
+echoecho -e "\e[1m\e[32m Developed by :\e[33m Tech Cochi"
